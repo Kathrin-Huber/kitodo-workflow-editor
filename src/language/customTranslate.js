8afacf046dc183b7ae315bbd17462c10ea19da88
Erik Sommer <Erik.Sommer@slub-dresden.de>
2020-09-02 18:34:22 +0200

    Cleanup

--- src/language/customTranslate.js
+++ src/language/customTranslate.js
@@ -2,32 +2,27 @@ import de from './de_DE';
 import en from './en_EN';
 
 export default function customTranslate(template, replacements) {
-    replacements = replacements || {};
-      
-    var availableLanguages = ["de", "en"];
-    
-    // switch (availableLanguages.indexOf(language)) {    
-    //     default: {
-    //         template = de[template] || template;
-    //         break; 
-    //     }
-    //     case 1: {
-    //         template = en[template] || template;
-    //         break;
-    //     }
-    // }
-    
-   var localizedString = de[template];
-   
-   if (localizedString == undefined) {
-     console.log(template);
-   }
-   
-   template = de[template] || template;
-   
-  
-    
-  return template.replace(/{([^}]+)}/g, function(_, key) {
-    return replacements[key] || '{' + key + '}';
-  });
+	replacements = replacements || {};
+
+	var localizedString = de[template];
+	if (localizedString == undefined || localizedString == ' ') {
+		console.log("Keine deutsche Übersetzung für '" + template + "' vorhanden!");
+	}
+
+	var availableLanguages = ["de", "en"];
+
+	switch (availableLanguages.indexOf(language)) {
+		default: {
+			template = de[template] || template;
+			break;
+		}
+		case 1: {
+			template = en[template] || template;
+			break;
+		}
+	}
+
+	return template.replace(/{([^}]+)}/g, function(_, key) {
+		return replacements[key] || '{' + key + '}';
+	});
 }
